b0c3ee0f5a8deb72e64fcda85b3e8d18adf96b6b
adrianoprog <adrianojoseadr@gmail.com>
2019-03-28 12:04:14 -0300

    usando lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild } from "@angular/core";
 import { Nav, Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
-import { HomePage } from "../pages/home/home";
 
 @Component({
   templateUrl: "app.html"
@@ -10,9 +9,9 @@ import { HomePage } from "../pages/home/home";
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: string = "HomePage";
 
-  pages: Array<{ title: string; component: any }>;
+  pages: Array<{ title: string; component: string }>;
 
   constructor(
     public platform: Platform,
@@ -21,22 +20,17 @@ export class MyApp {
   ) {
     this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [{ title: "Home", component: HomePage }];
+    this.pages = [{ title: "Home", component: "HomePage" }];
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
 }
